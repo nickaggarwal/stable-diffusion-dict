d56b8c10bf4ede727239ef1a98a466ca0e7f8f5d
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-18 12:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class InferlessPythonModel:
     def initialize(self):
         print("Hello World 8")
         self.pipe = StableDiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-2-1",
+            "runwayml/stable-diffusion-v1-5",
             use_safetensors=True,
             torch_dtype=torch.float16
         )
